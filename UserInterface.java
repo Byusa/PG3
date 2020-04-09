b7e37d233fdf31ffe45ea97990f3610701bf6eb2
Ahmed Azhar <ahmedazhar10@gmail.com>
2020-04-09 19:58:16 -0400

    Added User Interface file

---
+++ UserInterface.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class UserInterface {
+    public static void main(String[] args){ 
+        
+        // just after the 1 
+        System.out.println("Hello. Welcome to our Database!");
+        System.out.println("Enter the number for the following options");
+        System.out.println("1) *Option 1*");
+        System.out.println("2) *Option 2*");
+        System.out.println("3) *Option 3*");
+        System.out.println("4) *Option 4*");
+        System.out.println("5) *Option 5*");
+        System.out.println("6) Quit");
+    
+        Scanner input = new Scanner(System.in);
+        int number = input.nextInt();
+        while(number != 6){
+            System.out.println("Inside loop");
+            number = input.nextInt();
+        }
+        System.out.println("Bye!");
+        //String word = input.nextLine();
+        //System.out.println("You entered string "+word);
+        
+        
+    } 
+}
